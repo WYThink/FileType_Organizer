5133e2910af653aed1e6fbc493aa1390ba900012
WYThink <rajkrenil@gmail.com>
2024-11-27 13:30:14 +0530

    Formatted fileExtension.h

--- File_Extension_Detection/fileExtension.h
+++ File_Extension_Detection/fileExtension.h
@@ -14,16 +14,15 @@
 class extension : public sharedBuffer {
 private:
   std::string source_Folder_Path{};                       // Source Folder Path
-  std::string destination_Folder_Path{}; // Destination Folder Path
-  std::uintmax_t sourceSize{};           // Source Folder Size
-  std::uintmax_t totalFiles{};           // Calculate Total Files
-  sharedBuffer *buffer;                  // sharedBuffer Object Pointer
-  threadCreation *thread;                // threadCreation Object Pointer
+  std::string destination_Folder_Path{};                  // Destination Folder Path
+  std::uintmax_t sourceSize{};                            // Source Folder Size
+  std::uintmax_t totalFiles{};                            // Calculate Total Files
+  sharedBuffer *buffer;                                   // sharedBuffer Object Pointer
+  threadCreation *thread;                                 // threadCreation Object Pointer
 
 public:
-  const std::string_view source_FolderPathReturn(); // Return Source Folder Path
-  const std::string_view
-  destin_FolderPathReturn(); // Return Destination Folder Path
+  const std::string_view source_FolderPathReturn();       // Return Source Folder Path
+  const std::string_view destin_FolderPathReturn();       // Return Destination Folder Path
 
   void source_folder_Path_Input();                        // Path Input (Ask For Folder Path)
   void destination_folder_Path_Input();                   // Path Input (Ask For Folder Path)
