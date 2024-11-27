22fef9e8cc49e1355fd89c9a51987bcf37155d41
Krenil Raj <rajkrenil@gmail.com>
2024-11-27 10:56:30 +0530

    Update copyFiles.h
    ------

--- File_Extension_Detection/copyFiles.h
+++ File_Extension_Detection/copyFiles.h
@@ -14,21 +14,21 @@ class copyFiles
 {
 private:
 	sharedBuffer*						buffer;										//BUFFER Object
-	makeFolderClass*					makeFolderObject;							//makeFolderClass Object
-	int									bufSize{};									//Buffer Size
-	std::string							sourceFolderPath{};							//Source Folder Path
-	const std::vector<std::string>*		bufferPointer;                              //Const Buffer Pointer
-	const std::vector<std::string>*		newFolderPathPointer;						//Const Buffer "newFolderPath" Pointer
+	makeFolderClass*					makeFolderObject;								//makeFolderClass Object
+	int							bufSize{};									//Buffer Size
+	std::string						sourceFolderPath{};								//Source Folder Path
+	const std::vector<std::string>*				bufferPointer;                              					//Const Buffer Pointer
+	const std::vector<std::string>*				newFolderPathPointer;								//Const Buffer "newFolderPath" Pointer
 	
-	struct fullPath{																// Stores full paths of files for a specific extension.
-		std::vector<std::string>		pathStore{};								//Store Path
+	struct fullPath{															// Stores full paths of files for a specific extension.
+		std::vector<std::string>			pathStore{};									//Store Path
 	};
 
 public:
-	boost::thread copyFileThread;													//Thread Object
-	std::vector<struct fullPath> pathVar{};											//Vector of fullPath structs
+	boost::thread copyFileThread;														//Thread Object
+	std::vector<struct fullPath> pathVar{};													//Vector of fullPath structs
 	void filePathIterator();														//File Path Iterator
-	void finalyCopy();																//Copy File
+	void finalyCopy();															//Copy File
 	
 	//Constructor
 	copyFiles(sharedBuffer* sharedObject, const std::string_view sourcePath , makeFolderClass * makeFolderClassObject);
