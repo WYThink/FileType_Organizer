51ebdd4450cf7d0001ae8932a0200fec7ff62cb5
WYThink <rajkrenil@gmail.com>
2024-11-27 13:25:35 +0530

    Formatted fileExtension.h

--- File_Extension_Detection/fileExtension.h
+++ File_Extension_Detection/fileExtension.h
@@ -3,41 +3,39 @@
 #ifndef __FILEEXTENSION__
 #define __FILEEXTENSION__
 
-#include <iostream>
-#include <vector>
+#include "copyFiles.h"
 #include "sharedBuffer.h"
 #include "threadClass.h"
-#include "copyFiles.h"
+#include <iostream>
+#include <vector>
 
-//Extesion Class inherting properties from "sharedBuffer" Class and "threadCreation" Class
-class extension : public sharedBuffer 
-{
+// Extesion Class inherting properties from "sharedBuffer" Class and "threadCreation" Class
+class extension : public sharedBuffer {
 private:
-	std::string					source_Folder_Path{};			//Source Folder Path
-	std::string					destination_Folder_Path{};		//Destination Folder Path
-	std::uintmax_t				sourceSize{};					//Source Folder Size
-	std::uintmax_t				totalFiles{};					//Calculate Total Files
-	sharedBuffer*				buffer;							//sharedBuffer Object Pointer
-	threadCreation*				thread;							//threadCreation Object Pointer
+  std::string		source_Folder_Path{};						  // Source Folder Path
+  std::string		destination_Folder_Path{};					  // Destination Folder Path
+  std::uintmax_t	sourceSize{};								  // Source Folder Size
+  std::uintmax_t	totalFiles{};								  // Calculate Total Files
+  sharedBuffer		*buffer;									  // sharedBuffer Object Pointer
+  threadCreation	*thread;									  // threadCreation Object Pointer
 
 public:
-	
-	const std::string_view source_FolderPathReturn();			//Return Source Folder Path
-	const std::string_view destin_FolderPathReturn();			//Return Destination Folder Path
-	void source_folder_Path_Input();							//Path Input (Ask For Folder Path)
-	void destination_folder_Path_Input();						//Path Input (Ask For Folder Path)
-	void sizeCalc(const std::string_view sourceFolder);			//Folder Size Check
-	void allExtension(const std::string_view sourceFolder);		//Get All Extension
-	
-	//Prerequiste Checking 
-	void prerequiste_Check(const std::string_view folder_Path , int flag);
+  const std::string_view	source_FolderPathReturn();			  // Return Source Folder Path
+  const std::string_view	destin_FolderPathReturn();            // Return Destination Folder Path
+  
+  void source_folder_Path_Input();								  // Path Input (Ask For Folder Path)
+  void destination_folder_Path_Input();							  // Path Input (Ask For Folder Path)
+  void sizeCalc(const std::string_view sourceFolder);			  // Folder Size Check
+  void allExtension(const std::string_view sourceFolder);		  // Get All Extension
 
-	//Constructor 
-	extension(sharedBuffer* bufferObject);
-	
-	//Destructor
-	~extension();
+  // Prerequiste Checking
+  void prerequiste_Check(const std::string_view folder_Path, int flag);
 
+  // Constructor
+  extension(sharedBuffer *bufferObject);
+
+  // Destructor
+  ~extension();
 };
 
 #endif
