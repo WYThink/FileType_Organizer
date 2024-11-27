c3135fc3ce5c38043bd8c5b301bfa781c016a848
WYThink <rajkrenil@gmail.com>
2024-11-27 13:11:19 +0530

    Done

--- File_Extension_Detection/copyFiles.h
+++ File_Extension_Detection/copyFiles.h
@@ -3,38 +3,38 @@
 #ifndef __COPYFILES__
 #define __COPYFILES__
 
+#include "boost/thread.hpp"
+#include "makeFolderClass.h"
+#include "sharedBuffer.h"
 #include <iostream>
 #include <vector>
-#include "sharedBuffer.h"
-#include "makeFolderClass.h"
-#include "boost/thread.hpp"
 
-//Class for Copying Files 
-class copyFiles
-{
+// Class for Copying Files
+class copyFiles {
 private:
-    sharedBuffer*							buffer;								//Buffer Object
-    makeFolderClass*						makeFolderObject;					//makeFolderClass Object
-    int										bufSize{};							//Buffer Size
-    std::string								sourceFolderPath{};					//Source Folder Path
-    const std::vector<std::string>*			bufferPointer;						//Const Buffer Pointer
-    const std::vector<std::string>*			newFolderPathPointer;				//Const Buffer "newFolderPath" Pointer
+  sharedBuffer *buffer;                                                         // Buffer Object
+  makeFolderClass *makeFolderObject;                                            // makeFolderClass Object
+  int bufSize{};                                                                // Buffer Size
+  std::string sourceFolderPath{};                                               // Source Folder Path
+  const std::vector<std::string> *bufferPointer;                                // Const Buffer Pointer
+  const std::vector<std::string> *newFolderPathPointer;                         // Const Buffer "newFolderPath" Pointer
 
-    struct fullPath {															//Stores full paths of files for a specific extension.
-        std::vector<std::string>			pathStore{};						//Store Path
-    };
+  struct fullPath { // Stores full paths of files for a specific extension.
+    std::vector<std::string> pathStore{}; // Store Path
+  };
 
 public:
-    boost::thread copyFileThread;												//Thread Object
-    std::vector<struct fullPath> pathVar{};										//Vector of fullPath structs
-    void filePathIterator();													//File Path Iterator
-    void finalyCopy();															//Copy File
+  boost::thread copyFileThread;           // Thread Object
+  std::vector<struct fullPath> pathVar{}; // Vector of fullPath structs
+  void filePathIterator();                // File Path Iterator
+  void finalyCopy();                      // Copy File
 
-    //Constructor
-    copyFiles(sharedBuffer* sharedObject, const std::string_view sourcePath, makeFolderClass* makeFolderClassObject);
+  // Constructor
+  copyFiles(sharedBuffer *sharedObject, const std::string_view sourcePath,
+            makeFolderClass *makeFolderClassObject);
 
-    //Destructor
-    ~copyFiles();
+  // Destructor
+  ~copyFiles();
 };
 
 #endif // __COPYFILES__
